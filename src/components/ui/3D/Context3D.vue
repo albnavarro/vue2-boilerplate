0ee6282ff40a0a5a5fefaec005d7cc995606d7f3
albertonav <alberto.navaro@gmail.com>
2020-12-23 19:10:42 +0100

    [ Context3D ] add component to manage perspective

---
+++ src/components/ui/3D/Context3D.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="context3D" :style="setDeth">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Context3D',
+    props: {
+        depth: {
+            type: Number,
+            default: 1000
+        }
+    },
+    computed: {
+        setDeth() {
+            return {'perspective' : `${this.depth}px`}
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+.context3D {
+    transform-style: preserve-3d;
+
+    * {
+        transform-style: inherit;
+    }
+}
+
+</style>
